0198d0c1db07718286b9ceef7ab50a685c876f19
Mateusz <mateuszhotowy@gmail.com>
2017-12-01 14:48:12 +0100

    Dodano metode zwracającą tablicę obiektów Car na podstawie plików txt.

--- src/CarReader.java
+++ src/CarReader.java
@@ -1,8 +1,11 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class CarReader {
 
     public static File CarsFolder;
+    int carQuantity = 0;
 
     void createDir(){
          File newFile = new File("Cars");
@@ -13,15 +16,35 @@ public class CarReader {
     }
 
     void showCarsList(){
-        int i = 0;
+
         File folderList[] = CarsFolder.listFiles();
         System.out.println("LISTA SAMOCHODÓW: \n");
         for (File file: folderList) {
 
             System.out.println(file.toString());
-            i++;
+            carQuantity++;
         }
-        System.out.println("ILOSC SAMOCHODOW: "+i);
+        System.out.println("ILOSC SAMOCHODOW: "+carQuantity);
         System.out.println("\n");
+    }
+
+    Car[] loadCars() throws FileNotFoundException {
+        File folderList[] = CarsFolder.listFiles();
+        Car [] carObjectList = new Car[carQuantity];
+        int i = 0;
+        for (File file: folderList) {
+
+            Scanner sc = new Scanner(file);
+
+            String carProducer = sc.nextLine();
+            String carModel = sc.nextLine();
+            String licensePlates = sc.nextLine();
+
+            Car newCar = new Car(carProducer,carModel,licensePlates);
+            carObjectList[i] = newCar;
+            i++;
+
+        }
+        return carObjectList;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -7,26 +7,30 @@ public class Test {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
 
         CarReader cr = new CarReader();
        cr.createDir();
        cr.showCarsList();
 
-
-        System.out.println(CarReader.CarsFolder.getAbsolutePath());
-
+            Car[] carObjectList = cr.loadCars();
 
 
 
-        Car car1 = new Car("BMW","510d","DW550MM");
-        car1.createNewTxtFile();
-        car1.saveProperties();
+        System.out.println(carObjectList[0].model);
+        System.out.println(carObjectList[1].model);
 
-        Car car2 = new Car("Skoda","Fabia","DW103VC");
-        car2.createNewTxtFile();
-        car2.saveProperties();
+
+
+
+        //Car car1 = new Car("BMW","510d","DW550MM");
+        //car1.createNewTxtFile();
+        //car1.saveProperties();
+
+        //Car car2 = new Car("Skoda","Fabia","DW103VC");
+        //car2.createNewTxtFile();
+        //car2.saveProperties();
 
 
 
